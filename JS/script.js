387684067c3d5e63176a0806fefe55024d798e50
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-03-10 15:39:17 +0100

    fix

--- JS/script.js
+++ JS/script.js
@@ -6,17 +6,51 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.*/
 
 
 for (let i = 1 ; i<= 100; i++){
-    
 
-
-    if ( i % 3 === 0  ) {
+    if ( i % 5 === 0 && i % 3 === 0 ) {
 
        
         
         document.getElementById("cont-box").innerHTML +=
 
         `
-        <div class="box d-flex align-items-center justify-content-center ">
+        <div class="box d-flex align-items-center justify-content-center bg-danger fw-bold ">
+        
+        Fizzbuzz
+        
+        </div>
+    
+        `
+
+
+        
+        
+    }
+  else if ( i % 5 === 0  ) {
+
+       
+        
+        document.getElementById("cont-box").innerHTML +=
+
+        `
+        <div class="box d-flex align-items-center justify-content-center bg-warning fw-bold rounded-1  ">
+        
+        buzz
+        
+        </div>
+    
+        `
+
+
+        
+        
+    }
+  else if ( i % 3 === 0  ) {      
+        
+        document.getElementById("cont-box").innerHTML +=
+
+        `
+        <div class="box d-flex align-items-center justify-content-center fizz fw-bold rounded-1 ">
         
         fizz
         
@@ -25,23 +59,21 @@ for (let i = 1 ; i<= 100; i++){
         `
 
 
-        console.log (i)
+        
         
     }
-
-    else
-
-
-
-   console.log (i)
-    document.getElementById("cont-box").innerHTML +=
-
-    `
-    <div class="box d-flex align-items-center justify-content-center ">
-    
-    ${i}
-    </div>
-
-    `
+  else {
+        
+      //  console.log (i)
+        document.getElementById("cont-box").innerHTML +=
+        
+        `
+        <div class="box d-flex align-items-center justify-content-center bg-info fw-bold rounded-1 ">
+        
+        ${i}
+        </div>
+        
+        `
+    }
 
 }
